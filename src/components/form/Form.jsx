3543b54771aac4e188fc879c89daa6fe3f57e4ff
OlesiaPastushenko <olesya.pastushenko1@gmail.com>
2023-02-16 15:42:02 +0100

    user form added

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,12 +1,20 @@
 import "./form.css"
 import { useForm } from "react-hook-form";
+import { useRef, useState } from "react";
 export default function Form (){
+    
+        const onSubmit = (data) => {
+            console.log(data)
+            form.current.reset()
+        }
+    const { register, handleSubmit } = useForm();
+    const form = useRef()
     return (
-        <form className="form">
-            <input className="input" type='text' placeholder="Your name*"/>
-            <input className="input" type='email' placeholder="Your e-mail*"/>
-            <textarea className="textarea" placeholder="Your message*"/>
-            <input className="submitBtn" type='submit'/>
+        <form  ref={form} className="form" onSubmit={handleSubmit(onSubmit)}>
+            <input {...register('name')} className="input" type='text' placeholder="Your name*" required/>
+            <input  {...register('email')} className="input" type='email' placeholder="Your e-mail*" required/>
+            <textarea  {...register('message')}className="textarea" placeholder="Your message*" required/>
+            <button className="submitBtn" type='submit'>Send message</button>
         </form>
     )
 }
--- src/components/sectionRichOut/SectionRichOut.jsx
+++ src/components/sectionRichOut/SectionRichOut.jsx
@@ -1,8 +1,26 @@
 import Form from "../form/Form"
 import "./sectionRichOut.css"
+import map from "./imgRichOut/map.svg"
+import yellowBall from './imgRichOut/GroupYellow.png'
+import pinkBall from './imgRichOut/Group.png'
+import yellowBall2 from './imgRichOut/GroupYellow2.png'
+import ellipse from './imgRichOut/Ellipse.png'
+import cloud from './imgRichOut/cloud.svg'
 export default function SectionRichOut (){
     return (
         <div className="secRich">
+            <img src={map} className="map"/>
+            <img src={yellowBall} className="yellowBall"/>
+            <img src={ellipse} className="ellipse"/>
+            <img src={pinkBall} className="pinkBall"/>
+            <img src={yellowBall2} className="yellowBall2"/>
+            <img src={cloud} className="cl1"/>
+            <img src={cloud} className="cl2"/>
+            <img src={cloud} className="cl3"/>
+            <img src={cloud} className="cl4"/>
+            <img src={cloud} className="cl5"/>
+            <img src={cloud} className="cl6"/>
+            <img src={cloud} className="cl7"/>
             <div className="title">Reach out to us!</div>
             <Form/>
         </div>
